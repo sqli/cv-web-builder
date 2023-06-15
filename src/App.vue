34fd22028149d62897da888d51eed3c3eaa9e97e
Enrique Ferrando <enrique.ferrando@nestle.com>
2023-06-15 13:42:00 +0200

    added edit structure

--- src/App.vue
+++ src/App.vue
@@ -1,91 +1,96 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+import { ElMessageBox } from 'element-plus'
+
+const drawer = ref(false)
+const handleClose = (done: () => void) => {
+  ElMessageBox.confirm('Are you sure you want to close this?')
+    .then(() => {
+      done()
+    })
+    .catch(() => {
+      // catch error
+    })
+}
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/colors">theme colors</RouterLink>
-      </nav>
+  <el-affix :offset="0">
+    <div class="el-page-header__header top-menu">
+      <div class="el-page-header__left">
+        <div class="el-page-header__content">
+          <div class="flex items-center">
+            <el-dropdown>
+              <el-text class="mx-1" size="large"> Title </el-text>
+            </el-dropdown>
+            <el-dropdown>
+              <el-text class="mx-1" size="large">
+                Templates
+                <el-icon class="el-icon--right"><arrow-down /></el-icon>
+              </el-text>
+              <template #dropdown>
+                <el-dropdown-menu>
+                  <el-dropdown-item :icon="Plus">Action 1</el-dropdown-item>
+                  <el-dropdown-item :icon="CirclePlusFilled">
+                    Action 2
+                  </el-dropdown-item>
+                  <el-dropdown-item :icon="CirclePlus"
+                    >Action 3</el-dropdown-item
+                  >
+                  <el-dropdown-item :icon="Check">Action 4</el-dropdown-item>
+                  <el-dropdown-item :icon="CircleCheck"
+                    >Action 5</el-dropdown-item
+                  >
+                </el-dropdown-menu>
+              </template>
+            </el-dropdown>
+          </div>
+        </div>
+      </div>
+      <div class="el-page-header__extra">
+        <div class="flex items-center">
+          <el-button>Export to PDF</el-button>
+          <el-button type="primary" class="ml-2" @click="drawer = true"
+            >Edit</el-button
+          >
+        </div>
+      </div>
     </div>
-  </header>
+  </el-affix>
 
-  <RouterView />
+  <el-drawer v-model="drawer" direction="rtl" :before-close="handleClose">
+    <template #header="{ close, titleId, titleClass }">
+      <h4 :id="titleId" :class="titleClass">This is a custom header!</h4>
+      <el-button type="danger" @click="close">
+        <el-icon class="el-icon--left"><CircleCloseFilled /></el-icon>
+        Close
+      </el-button>
+    </template>
+    <span>Hi, there!</span>
+  </el-drawer>
+  <main><RouterView /></main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
+<style lang="scss" scoped>
+.top-menu {
+  background-color: white;
+  padding: 24px 0;
+  border-bottom: 1px solid var(--el-menu-border-color);
+  .el-dropdown + .el-dropdown {
+    margin-left: 12px;
+  }
+  .el-dropdown-link {
+    cursor: pointer;
+    color: var(--el-color-primary);
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    align-items: center;
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
+  .el-text {
+    outline: none;
   }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+}
+main {
+  margin-top: 24px;
 }
 </style>

505f16e3d9163895b2a4a92e4a035b1d0e3054d7
Enrique Ferrando <enrique.ferrando@nestle.com>
2023-06-23 12:34:51 +0200

    fix inline svg

--- src/components/InlineSvg.vue
+++ src/components/InlineSvg.vue
@@ -35,7 +35,7 @@ const props = defineProps({
   },
 })
 
-const innerSvg = ref(null)
+const innerSvg = ref<HTMLDivElement | null>(null)
 const isSvg = computed(() => {
   return props.src?.endsWith('.svg')
 })
@@ -47,19 +47,19 @@ const containerStyle = {
 const convertImageToSvg = async () => {
   try {
     const res = await fetch(props.src)
-    const svgText = await res.text()
+    const svgText = (await res.text()) as String
     writeSvg(svgText)
   } catch (error) {
     console.error(error)
   }
 }
 
-const writeSvg = (svgText) => {
+const writeSvg = (svgText: String) => {
   if (!innerSvg.value) return
 
   innerSvg.value.innerHTML = getSvgElement(svgText)
 
-  const svg = innerSvg.value.firstChild
+  const svg = innerSvg.value.firstChild as HTMLElement
 
   if (!svg || svg.tagName !== 'svg') return
 
@@ -68,19 +68,20 @@ const writeSvg = (svgText) => {
     role: props.ariaLabel && 'img',
     'aria-hidden': props.ariaHidden?.toString(),
     focusable: props.focusable?.toString(),
+    viewBox: '',
   }
 
-  const width = svg.getAttribute('width')
-  const height = svg.getAttribute('height')
-  const viewBox = svg.getAttribute('viewBox')
+  const width = (svg as Element).getAttribute('width')
+  const height = (svg as Element).getAttribute('height')
+  const viewBox = (svg as Element).getAttribute('viewBox')
   if (width && height && !viewBox) {
-    attribs['viewBox'] = `0 0 ${width} ${height}`
+    attribs.viewBox = `0 0 ${width} ${height}`
   }
 
   setAttributes(svg, attribs)
 }
 
-const setAttributes = (el, attrs) => {
+const setAttributes = (el: HTMLElement, attrs: any) => {
   Object.keys(attrs).forEach((key) => {
     if (attrs[key]) {
       el.setAttribute(key, attrs[key])
@@ -88,7 +89,7 @@ const setAttributes = (el, attrs) => {
   })
 }
 
-const getSvgElement = (svgText) => {
+const getSvgElement = (svgText: String) => {
   const svgPattern = /<\s*svg[^>]*>(.*?)<\/svg>/g
   const lineBreaksPattern = /(\r\n|\n|\r)/gm
   const matchResult = svgText.replace(lineBreaksPattern, '').match(svgPattern)

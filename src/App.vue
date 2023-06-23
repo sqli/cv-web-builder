d93ae428690aae8fb59b4ea61c00c74342979f91
Enrique Ferrando <enrique.ferrando@nestle.com>
2023-06-23 08:22:53 +0200

    added default template and pdf export

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,7 @@ const store = useSchema()
 // aqui pillariamos el data de la url
 
 const drawer = ref(false)
+const loading = ref(false)
 const router = useRouter()
 const route = useRoute()
 
@@ -26,6 +27,8 @@ const print = () => {
   const a4Width = 210 // paper Standard Width
   const a4Height = 297 // paper Standard Height
   const paper = document.querySelector('.page')
+  loading.value = true
+  document.querySelector('#app').classList.add('html2canvas')
 
   html2canvas(paper, {
     scale: 2,
@@ -48,75 +51,80 @@ const print = () => {
       heightLeft -= pageHeight
     }
     doc.save('Downld.pdf')
+    document.querySelector('#app').classList.remove('html2canvas')
+    loading.value = false
   })
 }
 </script>
 
 <template>
-  <el-affix :offset="0" class="top-menu">
-    <div class="el-page-header__header">
-      <div class="el-page-header__left">
-        <div class="el-page-header__content">
+  <div v-loading="loading">
+    <el-affix :offset="0" class="top-menu">
+      <div class="el-page-header__header">
+        <div class="el-page-header__left">
+          <div class="el-page-header__content">
+            <div class="flex items-center">
+              <el-text class="mx-1" size="large"> CV Web Builder </el-text>
+            </div>
+          </div>
+        </div>
+        <div class="el-page-header__extra">
           <div class="flex items-center">
-            <el-text class="mx-1" size="large"> CV Web Builder </el-text>
+            <el-dropdown class="template-selector" @command="handleCommand">
+              <el-text class="mx-1" size="large">
+                Templates
+                <el-icon class="el-icon--right"><arrow-down /></el-icon>
+              </el-text>
+              <template #dropdown>
+                <el-dropdown-menu>
+                  <el-dropdown-item command="Default">Default</el-dropdown-item>
+                  <el-dropdown-item command="Hello">Hello</el-dropdown-item>
+                </el-dropdown-menu>
+              </template>
+            </el-dropdown>
+            <el-button @click="print">Export to PDF</el-button>
+            <el-button type="primary" class="ml-2" @click="drawer = true"
+              >Edit</el-button
+            >
           </div>
         </div>
       </div>
-      <div class="el-page-header__extra">
-        <div class="flex items-center">
-          <el-dropdown class="template-selector" @command="handleCommand">
-            <el-text class="mx-1" size="large">
-              Templates
-              <el-icon class="el-icon--right"><arrow-down /></el-icon>
-            </el-text>
-            <template #dropdown>
-              <el-dropdown-menu>
-                <el-dropdown-item command="Default">Default</el-dropdown-item>
-                <el-dropdown-item command="Hello">Hello</el-dropdown-item>
-              </el-dropdown-menu>
-            </template>
-          </el-dropdown>
-          <el-button @click="print">Export to PDF</el-button>
-          <el-button type="primary" class="ml-2" @click="drawer = true"
-            >Edit</el-button
-          >
-        </div>
-      </div>
-    </div>
-  </el-affix>
+    </el-affix>
 
-  <el-drawer v-model="drawer" direction="rtl" class="form-drawer">
-    <form-schema></form-schema>
-  </el-drawer>
-  <main><RouterView /></main>
+    <el-drawer v-model="drawer" direction="rtl" class="form-drawer">
+      <form-schema></form-schema>
+    </el-drawer>
+    <main><RouterView /></main>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 .top-menu {
-  background-color: white;
-  padding: 24px;
-  border-bottom: 1px solid var(--el-menu-border-color);
-  margin: 0 auto;
-  .el-dropdown + .el-dropdown {
-    margin-left: 12px;
-  }
-  .el-dropdown-link {
-    cursor: pointer;
-    color: var(--el-color-primary);
-    display: flex;
-    align-items: center;
-  }
-  .el-text {
-    outline: none;
-  }
-  .template-selector {
-    padding: 4px 12px;
-  }
-  @media print {
-    display: none;
+  .el-page-header__header {
+    background-color: white;
+    padding: 24px;
+    border-bottom: 1px solid var(--el-menu-border-color);
+    .el-dropdown + .el-dropdown {
+      margin-left: 12px;
+    }
+    .el-dropdown-link {
+      cursor: pointer;
+      color: var(--el-color-primary);
+      display: flex;
+      align-items: center;
+    }
+    .el-text {
+      outline: none;
+    }
+    .template-selector {
+      padding: 4px 12px;
+    }
+    @media print {
+      display: none;
+    }
   }
 }
 main {
-  margin-top: 24px;
+  padding-top: 24px;
 }
 </style>

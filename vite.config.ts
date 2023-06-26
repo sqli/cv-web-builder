16d402c3ed4cd1fd7b8efba1fbd2843af9fa68f4
Enrique Ferrando <enrique.ferrando@nestle.com>
2023-06-26 12:47:18 +0200

    test config 3

--- vite.config.ts
+++ vite.config.ts
@@ -15,5 +15,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  //base: process.env.NODE_ENV === 'production' ? '/cv-web-builder/' : './',
+  base: process.env.NODE_ENV === 'github-pages' ? '/cv-web-builder/' : './',
 })
